3206d9e04b52683a736236fee9d14315c4658b50
S4ika <s4ikait@gmail.com>
2022-02-09 00:28:38 +0300

    Fav_lang

--- 1 lab/lab1.rb
+++ 1 lab/lab1.rb
@@ -1,1 +1,17 @@
-puts "Hello, " + ARGV[0]
+#puts "Hello, " + ARGV[0]
+#ARGV.clear
+#Task 3
+print "Какой любимый язык? "
+lang = gets.chomp
+case lang
+	when "Ruby"
+		puts "Подлиза"
+	when "C++"
+		puts "Бедолага"
+	when "C#"
+		puts "Ну Ruby, так Ruby"
+	when "Python"
+		puts "Это как Ruby, только хуже"
+	else
+		puts "Хороший выбор - Ruby! А твой выбор плох =)"
+	end
