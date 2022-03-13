f7793d471d1ca3c66a5277edf38980daba8008d1
S4ika <s4ikait@gmail.com>
2022-03-13 22:23:47 +0300

    Block 2 Tasks 1-2

--- 1 lab/lab1.rb
+++ 1 lab/lab1.rb
@@ -18,13 +18,77 @@
 #	end
 
 
-puts("Кручу-верчу, обмануть хочу! Угадай в каком из 5 стаканов вода")
-num = gets.to_i
-if num <= 6
-	puts("В этот раз не повезло(\nЕще попытка 100 у.е.")
-else 
-	puts("Так всего 5 стаканов, куда такой номер?")
+#puts("Кручу-верчу, обмануть хочу! Угадай в каком из 5 стаканов вода")
+#num = gets.to_i
+#if num <= 6
+#	puts("В этот раз не повезло(\nЕще попытка 100 у.е.")
+#else 
+#	puts("Так всего 5 стаканов, куда такой номер?")
+#end
+#unless num > 5
+#	puts("В этот раз не повезло(\nЕще попытка 100 у.е.")
+#end
+#Task 4
+puts "Input Ruby command..."
+cmd = gets.chomp
+puts "Result your command :\n"
+eval(cmd)
+
+puts "Input OS command..."
+cmd_os = gets.chomp
+puts "Result your command :\n"
+exec(cmd_os)
+
+#Task 2
+#1.
+num = ARGV[0].to_i
+# sum_digits = 0
+# while num !=0
+	# sum_digits += num % 10
+	# num /= 10
+# end
+# puts sum_digits
+#2.
+
+def sum_digits(number)
+	sum = 0
+	while number != 0
+		sum += number%10
+		number /= 10
+	end
+	sum
 end
-unless num > 5
-	puts("В этот раз не повезло(\nЕще попытка 100 у.е.")
+
+def mult_digits(number)
+	res = 1
+	while number != 0
+		res *= number%10
+		number /= 10
+	end
+	res
 end
+
+def find_max(number)
+	max = number%10
+	number /= 10
+	while number != 0
+		check = number % 10
+		if check > max max = check
+		number /= 10
+	end
+	max
+end
+
+
+def find_min(number)
+	min_el = number % 10
+	number /= 10
+	while number != 0
+		if min_el > number % 10
+			min_el = number % 10
+		end
+		number /= 10
+	end
+	min_el
+end
+
